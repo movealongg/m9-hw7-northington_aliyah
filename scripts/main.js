0abb64c4760c9ef4a560fd53b3441f1e7605833a
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2018-10-20 22:31:20 -0500

    Add event listener mouseenter/mouseleave to tabs

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ var tab = document.getElementsByTagName('a');
 //This is all links on page
 
 function changeColor(){
-	
+
 };
 
 for (i=0; i < tab.length; i++){
-	
-}
+	tab[3].addEventListener('mouseenter', changeColor);
+	tab[3].addEventListener('mouseleave', changeColor);
+	tab[4].addEventListener('mouseenter', changeColor);
+	tab[4].addEventListener('mouseleave', changeColor);
+};
