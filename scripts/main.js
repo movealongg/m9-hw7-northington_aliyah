14be6f35c865118985dfbc8d47bc1a4376472397
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2018-10-21 18:43:17 -0500

    Add location dropdown to find a location tab
    A variable was created for the hide-loc tab.
    
    A function was created to toggle the show-loc tab.
    
    A click event listener was added to find a location tab.
    
    hide-loc and show-loc css classes were created for location dropdown.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 var tab = document.getElementsByTagName('a');
 //This is all links on page
 
-function changeColor(){
+var locations = document.getElementsByClassName('hide-loc');
+
+function changeColor() {
 	this.classList.toggle('change-color');
+};
+
+function showLocations() {
+	this.nextElementSibling.classList.toggle('show-loc');
 };
 
 for (i=0; i < tab.length; i++){
@@ -10,4 +16,5 @@ for (i=0; i < tab.length; i++){
 	tab[3].addEventListener('mouseleave', changeColor);
 	tab[4].addEventListener('mouseenter', changeColor);
 	tab[4].addEventListener('mouseleave', changeColor);
+	tab[3].addEventListener('click', showLocations);
 };
